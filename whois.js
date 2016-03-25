57bed0b1dbbe4552557ca747a854c1442b90e8f2
Mike <spectre.harvey@gmail.com>
2016-03-25 13:31:17 -0500

    Added whois

---
+++ whois.js
@@ -0,0 +1,4 @@
+/*
+ * When you want to lookup the whois on the current page's domain.
+ */
+javascript:(function(){window.location.href='http://who.is/whois/'+window.location.hostname.toString();})();
