67024ef898bd9a6731b55bbde5157f97ee6200c0
Mike Kelly <MikeKellyWeb@gmail.com>
2017-10-12 13:35:26 -0500

    Update simple-scroll.js
    Whoops!

--- simple-scroll.js
+++ simple-scroll.js
@@ -1,4 +1,4 @@
 /*
  * Sometimes when I'm back and forth between vim and a website, I use j/k to scroll and I get frustrated. Let's fix that!
  */
-javascript:(function(){document.onkeypress=function(e){e=e||window.event;var cCode=e.keyCode||e.which;var cStr=String.fromCharCode(cCode);if(document.activeElement.tagName==='BODY'){if(cStr==='j'){window.scrollBy(0,100);}else if(cStr==='k'){window.scrollBy(0,-100);}};})();
+javascript:(function(){document.onkeypress=function(e){e=e||window.event;var cCode=e.keyCode||e.which;var cStr=String.fromCharCode(cCode);if(document.activeElement.tagName==='BODY'){if(cStr==='j'){window.scrollBy(0,100);}else if(cStr==='k'){window.scrollBy(0,-100);}};}})();
